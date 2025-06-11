d165a4a1bed5a5d210cc7338ed30f8235d64351b
ghoste9624 <jackspade44@outlook.com>
2025-06-10 22:02:24 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+pip install --upgrade pip
+pip install requests 
+pip install bs4
+pip install linkchecker 
+pip install google 
+pip install sslscan
+pip install webtech
+pip install thorndyke 
+pip install linkook
+pip install search4
+pip install holehe 
+pip install phonenumbers
+pip install geopy
+pip install speedtest-cli
